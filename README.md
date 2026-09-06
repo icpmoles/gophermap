9fb781d75a7522bff395fba1f7c86e5d96d9f88e
Iacopo Moles <iacopo@icpmol.es>
2026-09-06 13:22:28 +0200

    minimum viable product
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>

---
+++ README.md
@@ -0,0 +1,9 @@
+## Usage
+
+
+
+
+## Credits
+
+sitemap template taken from:
+https://github.com/gohugoio/hugo/blob/master/tpl/tplimpl/embedded/templates/sitemap.xml 
--- cli/main.go
+++ cli/main.go
@@ -2,42 +2,102 @@ package main
 
 import (
 	"embed"
-	// "fmt"
+	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"slices"
+	"strings"
 	"text/template"
+	"time"
 )
 
 //go:embed assets/*
 var content embed.FS
 
+type File struct {
+	Name    string
+	LastMod time.Time
+}
+
 type Folder struct {
-	files   []string
+	name    string
+	files   []File
 	folders []Folder
 }
 
-func getSitemap(wr io.Writer, path string) error {
+type FlattenedFolder struct {
+	Files []File
+}
+
+func isAllowedFile(path string) bool {
+	allowList := []string{"pdf", "txt", "epub", "md"}
+
+	ext := strings.TrimPrefix(filepath.Ext(path), ".")
+
+	return slices.Contains(allowList, ext)
+}
+
+func getFlattenedFolder(path string) (ff FlattenedFolder, err error) {
+
+	err = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() && isAllowedFile(path) {
+			info, err := d.Info()
+			if err != nil {
+				return err
+			}
+
+			file := File{Name: path, LastMod: info.ModTime()}
+			// fmt.Println(path, info.ModTime())
+			ff.Files = append(ff.Files, file)
+		}
+
+		return nil
+	})
+
+	return ff, err
+}
+
+func CreateSitemap(wr io.Writer, path string) (time.Duration, error) {
+	start := time.Now()
+	fmt.Println("Exploring ", path)
+	files, _ := getFlattenedFolder(path)
 
 	ts, err := template.ParseFS(content, "assets/sitemap.tmpl.xml")
 	if err != nil {
 		log.Print(err.Error())
 	}
 
-	err = ts.Execute(wr, nil)
+	err = ts.Execute(wr, files)
 
-	return err
+	return time.Since(start), err
 }
 
 func main() {
-	s_f, err := os.Create("sitemap.xml")
-	if err != nil {
-		log.Print(err.Error())
+	folder := "."
+	if len(os.Args) < 2 {
+		fmt.Println("Writing in the same directory")
+	} else {
+		folder = os.Args[1]
 	}
 
-	err = getSitemap(s_f, "./target")
+	s_f, err := os.Create("sitemap.xml")
 	if err != nil {
-		log.Print(err.Error())
+		log.Fatal(err.Error())
+		os.Exit(1)
 	}
-	// fmt.Println("Hello, World!")
+
+	execution_time, err := CreateSitemap(s_f, folder)
+
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Template Executed in: ", execution_time)
 }
