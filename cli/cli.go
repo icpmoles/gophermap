1de733f507e6cd896cc31b0212b925ed1a36cb33
Iacopo Moles <iacopo@icpmol.es>
2026-09-10 16:53:33 +0200

    initial support for custom allowed file extensions
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>

--- cli/cli.go
+++ cli/cli.go
@@ -6,6 +6,7 @@ import (
 	"gophermap/gophermap"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -23,13 +24,33 @@ func print_usage() {
 	flag.PrintDefaults()
 }
 
+type cliStringList []string
+
+func (s *cliStringList) String() string {
+	return strings.Join(*s, ",")
+}
+
+func (s *cliStringList) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
 func main() {
 	/* Parse CLI parameters*/
 	folderPtr := flag.String("directory", ".", "Directory to analyze")
 	sitemap := flag.String("o", "sitemap.xml", "output file")
 
+	// we allow multiple allowed extensions
+	var allowed cliStringList
+	flag.Var(&allowed, "allow", "allowed extension (can be specified multiple times) (default 'md','pdf','txt','md')")
+
 	flag.Usage = print_usage
 	flag.Parse()
+
+	if len(allowed) == 0 {
+		// default values
+		allowed = []string{"pdf", "txt", "epub", "md"}
+	}
 
 	args := flag.Args()
 	if len(args) != 1 {
@@ -44,7 +65,7 @@ func main() {
 	}
 
 	start := time.Now()
-	err = gophermap.CreateSitemap(s_f, *folderPtr, url, []string{"pdf", "txt", "epub", "md"})
+	err = gophermap.CreateSitemap(s_f, *folderPtr, url, allowed)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
